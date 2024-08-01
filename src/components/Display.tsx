a5f5c0b4d648fa3e4152dba5a9da3d65041bf770
yvanddniyo <nyvan80076@gmail.com>
2024-08-01 23:20:44 +0200

    adding new changes on the FE parts

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,14 +1,25 @@
 import { Route, Routes, useLocation } from "react-router-dom"
 import DisplayHome from "./DisplayHome"
 import DisplayAlbum from "./DisplayAlbum"
-import { useRef } from "react"
+import { useEffect, useRef } from "react"
+import { albumsData } from "../assets/frontend-assets/assets"
 
 const Display = () => {
   const displayRef:any = useRef()
   const location = useLocation()
   const isAlbum = location.pathname.includes("album")
   const albumId = isAlbum ? location.pathname.slice(-1) : ""
-  console.log(albumId);
+  const bgColor = albumsData[Number(albumId)].bgColor
+  console.log(bgColor);
+
+  useEffect(() => {
+    if (isAlbum) {
+      displayRef.current.style.background = `linear-gradient(${bgColor}, #121212)`
+    }
+    else {
+      displayRef.current.style.background =  `#121212)`
+    }
+  })
   
   return (
     <div ref={displayRef} className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
